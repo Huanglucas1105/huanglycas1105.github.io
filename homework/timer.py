50ef92fe2c5199d66e77b6352a80f09cd0a718a4
Huanglucas1105 <120691497+Huanglucas1105@users.noreply.github.com>
2022-12-31 14:07:53 +0800

    update

--- homework/timer.py
+++ homework/timer.py
@@ -1,14 +1,32 @@
 import time
 import os
 
-input_TM = 0
+clock = 0
 is_break = 1
 
 def timer():
-    global input_TM, is_break
+    global clock, is_break
     try:
-        input_TM = str(input('設定時間'))
-        if input_TM == 0:
+        clock = str(input('設定時間'))
+        if clock == 0:
             is_break = True
 
     except ValueError:
+        print('再試一次')
+        timer()
+
+def clearscreen():
+    os.command('cls')
+
+def timercount():
+    global clock
+    for i in range(clock):
+        time.sleep(1)
+        print(clock - i)
+    clearscreen()
+while True:
+    clearscreen()
+    timer()
+    if is_break == True:
+        break
+    timercount()
