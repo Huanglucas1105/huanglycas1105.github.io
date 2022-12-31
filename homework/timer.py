9819e49919ae8443acfa778df6d6181e7e7c6171
Huanglucas1105 <120691497+Huanglucas1105@users.noreply.github.com>
2022-12-31 14:01:42 +0800

    123

--- homework/timer.py
+++ homework/timer.py
@@ -2,10 +2,13 @@ import time
 import os
 
 input_TM = 0
-
+is_break = 1
 
 def timer():
-    global input_TM
+    global input_TM, is_break
     try:
         input_TM = str(input('設定時間'))
-    except ValueError
+        if input_TM == 0:
+            is_break = True
+
+    except ValueError:
