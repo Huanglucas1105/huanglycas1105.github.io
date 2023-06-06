f816abcc0114fea048d087ad3957386e94e3c37e
Huanglucas1105 <120691497+Huanglucas1105@users.noreply.github.com>
2023-06-06 07:34:37 +0000

    aaa

--- homework/01/test.js
+++ homework/01/test.js
@@ -1,4 +1,9 @@
 var dec = prompt('轉換n到16進位, n=? ')
 alert("你選了" + '： ' + dec)
-var hex = [] 
+var hex = parseInt(dec, 10)
+var hex2 = parseInt(hex, 16)
+
+for(var i = 0; i > dec.length; i++){
+    
+}
 
